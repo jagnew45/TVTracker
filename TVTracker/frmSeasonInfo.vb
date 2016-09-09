dc01504a6d84f46e9892e932ee9c7c8ec58af5ae
jagnew45 <jagnew45@gmail.com>
2016-09-09 11:00:05 +1000

    Update frmSeasonInfo.vb
    Added notes and replaced references to txtSeasonNum with the label replacement.

--- TVTracker/frmSeasonInfo.vb
+++ TVTracker/frmSeasonInfo.vb
@@ -13,20 +13,11 @@
         Dim result As Boolean = True
         msg = "The form cannot submit until the following errors are fixed: " + vbNewLine
 
-        'Validate Season Number
-        If txtSeasonNum.Text = "" Then
-            msg = msg + "Season Number must have a value" + vbNewLine
-            result = False
-        ElseIf txtSeasonNum.Text <> "" And IsNumeric(txtSeasonNum.Text) = False Then
-            msg = msg + "Season Number must be a number" + vbNewLine
-            result = False
-        End If
-
         'Validate Episode Count
         If txtEpisodeCount.Text = "" Then
             msg = msg + "Episodes in Season must have a value" + vbNewLine
             result = False
-        ElseIf txtSeasonNum.Text <> "" And IsNumeric(txtEpisodeCount.Text) = False Then
+        ElseIf txtEpisodeCount.Text <> "" And IsNumeric(txtEpisodeCount.Text) = False Then
             msg = msg + "Episodes in Season must be a number" + vbNewLine
             result = False
         End If
@@ -41,16 +32,27 @@
     Private Sub btnAddInfo_Click(sender As Object, e As EventArgs) Handles btnAddInfo.Click
         'if validation is successful, store values in arraylist
         If validate() = True Then
-            list.Add(txtSeasonNum.Text & ", " & txtEpisodeCount.Text) 'Values read in on one line (can be immediately used for INSERT - just need to add show_id to the front)
+            'Values read in on one line (can be immediately used for INSERT - just need to add show_id to the front)
+            list.Add(lblSeasonNum.Text & ", " & txtEpisodeCount.Text) 
         End If
-        Dim line As String
-        For Each line In list
-            MsgBox(line)
-        Next
+        
+       
 
     End Sub
 
+'This button should go to the next screen; NOT back to the addshow screen
     Private Sub btnReturntoAdd_Click(sender As Object, e As EventArgs) Handles btnReturntoAdd.Click
+        
+        'NOTE*** This should be a collective insert loop once the user is ready to proceed
+        Dim line As String
+        For Each line In list
+            MsgBox(line)
+            'INSERT ...
+        Next
+    
+    'ONly once the inserts are successful, should this proceed the user to the next screen... confirmation?
+    'If you're doing a confrimation screen, then all of the data should be stored in variables until it is confirmed...
+    
         frmAddShow.Show()
         Me.Close()
     End Sub
